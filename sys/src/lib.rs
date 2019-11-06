60cbf42c59c51307a172c52ab589c13770c1efb8
phR0ze <prc451@gmail.com>
2019-11-06 09:35:51 -0700

    0.0.7: Use AsRef to handle more types

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -5,20 +5,20 @@ use std::path::{Path, PathBuf};
 // Returns the full path to the directory of the current running executable.
 pub fn exec_dir() -> io::Result<PathBuf> {
     let path = env::current_exe()?;
-    let dir = dirname(path.as_path())?;
+    let dir = dirname(&path)?;
     Ok(dir)
 }
 
 // Returns the `Path` without its final component, if there is one.
-pub fn dirname(path: &Path) -> io::Result<PathBuf> {
-    let parent = path.parent().ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Parent directory not found"))?;
+pub fn dirname<T: AsRef<Path>>(path: &T) -> io::Result<PathBuf> {
+    let parent = path.as_ref().parent().ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Parent directory not found"))?;
     let dir = parent.to_path_buf();
     Ok(dir)
 }
 
 // Returns the final component of the `Path`, if there is one.
-pub fn filename(path: &Path) -> io::Result<&str> {
-    let os_str = path.file_name().ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Filename not found"))?;
+pub fn filename<T: AsRef<Path>>(path: &T) -> io::Result<&str> {
+    let os_str = path.as_ref().file_name().ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Filename not found"))?;
     let filename = os_str.to_str().ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Unable to convert filename into String"))?;
     Ok(filename)
 }
@@ -32,5 +32,17 @@ mod tests {
         let cwd = env::current_dir().unwrap();
         let dir = cwd.parent().unwrap().join("target/debug/deps");
         assert_eq!(dir, exec_dir().unwrap())
+    }
+
+    #[test]
+    fn test_dirname() {
+        let dir = PathBuf::from("/foo/bar");
+        assert_eq!(PathBuf::from("/foo"), dirname(&dir).unwrap())
+    }
+
+    #[test]
+    fn test_filename() {
+        let path = PathBuf::from("/foo/bar");
+        assert_eq!("bar", filename(&path).unwrap())
     }
 }
