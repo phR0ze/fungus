f7bc034793f775ee279c940c61b2e1b92cfaddbd
phR0ze <prc451@gmail.com>
2019-11-06 11:23:19 -0700

    0.0.8: Implement dirname and filename as PathBuf extensions

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -2,6 +2,27 @@ use std::env;
 use std::io;
 use std::path::{Path, PathBuf};
 
+// PathBuf extensions
+pub trait PathBufExt {
+    fn dirname(&self) -> io::Result<PathBuf>;
+    fn filename(&self) -> io::Result<&str>;
+}
+impl PathBufExt for PathBuf {
+    // Returns the `Path` without its final component, if there is one.
+    fn dirname(&self) -> io::Result<PathBuf> {
+        let parent = self.as_path().parent().ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Parent directory not found"))?;
+        let dir = parent.to_path_buf();
+        Ok(dir)
+    }
+
+    // Returns the final component of the `Path`, if there is one.
+    fn filename(&self) -> io::Result<&str> {
+        let os_str = self.as_path().file_name().ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Filename not found"))?;
+        let filename = os_str.to_str().ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Unable to convert filename into String"))?;
+        Ok(filename)
+    }
+}
+
 // Returns the full path to the directory of the current running executable.
 pub fn exec_dir() -> io::Result<PathBuf> {
     let path = env::current_exe()?;
@@ -31,18 +52,30 @@ mod tests {
     fn test_exec_dir() {
         let cwd = env::current_dir().unwrap();
         let dir = cwd.parent().unwrap().join("target/debug/deps");
-        assert_eq!(dir, exec_dir().unwrap())
+        assert_eq!(dir, exec_dir().unwrap());
+    }
+
+    #[test]
+    fn test_pathbufext_dirname() {
+        let dir = PathBuf::from("/foo/bar");
+        assert_eq!(PathBuf::from("/foo"), dir.dirname().unwrap());
+    }
+
+    #[test]
+    fn test_pathbufext_filename() {
+        let path = PathBuf::from("/foo/bar");
+        assert_eq!("bar", path.filename().unwrap());
     }
 
     #[test]
     fn test_dirname() {
         let dir = PathBuf::from("/foo/bar");
-        assert_eq!(PathBuf::from("/foo"), dirname(&dir).unwrap())
+        assert_eq!(PathBuf::from("/foo"), dirname(&dir).unwrap());
     }
 
     #[test]
     fn test_filename() {
         let path = PathBuf::from("/foo/bar");
-        assert_eq!("bar", filename(&path).unwrap())
+        assert_eq!("bar", filename(&path).unwrap());
     }
 }
