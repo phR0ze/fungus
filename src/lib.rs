47a5c5d0ab299dbd684dd9b758b5c66ac287b134
phR0ze <prc451@gmail.com>
2019-11-06 21:55:17 -0700

    0.0.13: Implemented stdio for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,33 @@
+use std::io;
+
+// Substitute stdout and stderr for testing
+pub struct Stdio<T: io::Write, U: io::Write> {
+    pub out: T,
+    pub err: U,
+}
+impl<T: io::Write, U: io::Write> Stdio<T, U> {
+    pub fn new(out: T, err: U) -> Self {
+        Stdio { out, err }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use sys::*;
+    use super::*;
+    use std::io::{self, Write};
 
     #[test]
-    fn it_works() {
-        for x in getpaths(&"*").unwrap() {
-            println!("{:?}", x)
-        }
+    fn test_stdio() {
+        // real
+        let mut stdio = Stdio::new(io::stdout(), io::stderr());
+        writeln!(stdio.out, "Hello out").unwrap();
+        writeln!(stdio.err, "Hello err").unwrap();
+
+        // buffer
+        let mut stdio = Stdio::new(Vec::new(), Vec::new());
+        writeln!(stdio.out, "Hello out").unwrap();
+        writeln!(stdio.err, "Hello err").unwrap();
+        assert_eq!(stdio.out, b"Hello out\n");
+        assert_eq!(stdio.err, b"Hello err\n");
     }
 }
