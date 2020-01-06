69ffb85c4e83add9b093c623daaf02fbae60d205
phR0ze <prc451@gmail.com>
2020-01-06 16:57:47 -0700

    0.0.55: Working on arch linux abs tooling

--- src/arch/abs.rs
+++ src/arch/abs.rs
@@ -1,71 +1,87 @@
 // Arch Linux ABS
 // https://wiki.archlinux.org/index.php/Arch_Build_System
 // https://wiki.archlinux.org/index.php/Arch_Build_System#Retrieve_PKGBUILD_source_using_Git
-//
-// # Clone the svntogit repo for a specific package using https://github.com/archlinux/asp
-// # asp replaces the abs tool, offering more up to date sources (via the svntogit repositories)
-// # using a sparse checkout to cache at ${XDG_CACHE_HOME:-$HOME/.cache}/asp.
-// $ asp checkout <pkgname>
-//
-// asp calls:
-// $ git init -q ${XDG_CACHE_HOME:-$HOME/.cache}/asp
-// $ git remote add "packages" "https://git.archlinux.org/svntogit/packages.git
-// $ git remote add "community" "https://git.archlinux.org/svntogit/community.git
-// $ git branch -qf "$branchname" "refs/remotes/$branchname"
-// $ git branch -qf "$branchname" "refs/remotes/$branchname"
 use git2::build::RepoBuilder;
-use git2::Repository;
 
 use crate::prelude::*;
+use failure::bail;
+
+const REPO_PACKAGES_NAME: &str = "packages";
+const REPO_COMMUNITY_NAME: &str = "community";
+
+// An repo identifier
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum Repo {
+    /// Arch Linux packages repository
+    Packages,
+
+    /// Arch Linux community repository
+    Community,
+}
 
 #[cfg(feature = "_arch_")]
 pub fn init<T: AsRef<Path>>(path: T) -> Result<()> {
+    let pkg = "pkgfile".to_string();
     let path = path.as_ref().abs()?;
 
     // Ensure the destination dir exists
     sys::mkdir(&path)?;
 
     // Init the repo at the given destination dir
-    let repo = Repository::init(&path)?;
+    let mut found = false;
+    let mut repo: git2::Repository;
+    for name in vec![REPO_PACKAGES_NAME, REPO_COMMUNITY_NAME] {
+        // Configure the branch to checkout
+        let branch = format!("{}/{}", &name, &pkg);
+        let mut builder = RepoBuilder::new();
+        builder.branch(&branch);
 
-    // Create the refspec for our target branch
-    let branch = "packages/pkgfile".to_string();
-    let refspec = format!("+refs/heads/{0:}:refs/remotes/origin/{0:}", &branch);
+        // Create the remote with the --single-branch refspec override so that when clone is
+        // called only the indicated branch is downloaded from the server not the whole repo.
+        builder.remote_create(|repo, name, url| {
+            let refspec = format!("+refs/heads/{0:}:refs/remotes/origin/{0:}", &branch);
+            repo.remote_with_fetch(name, url, &refspec)
+        });
 
-    // Add the target remotes
-    //for x in &vec!["packages", "community"] {
-    for x in &vec!["packages"] {
-        // Add the remote
-        let mut remote = repo.remote(x, &format!("https://git.archlinux.org/svntogit/{}.git", x))?;
-
-        // Fetch the remote branch via a refspec
-        remote.fetch(&[&refspec], None, None).unwrap();
-
-        //repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))?;
-        let head = repo.find_reference("FETCH_HEAD")?;
-        let oid = head.target().unwrap();
-        let commit = repo.find_commit(oid)?;
-        repo.branch(&branch, &commit, false)?;
-        // println!("{:?}", oid);
-        println!("foo");
+        // Clone the single branch from teh repo
+        match builder.clone(&format!("https://git.archlinux.org/svntogit/{}.git", &name), &path) {
+            Ok(x) => {
+                found = true;
+                repo = x
+            }
+            Err(_) => continue,
+        }
     }
 
-    // Checkout the branch
-    //let builder = git2::build::CheckoutBuilder::new();
-    //repo.checkout_head(opts: Option<&mut CheckoutBuilder<'_>>)
-    // let head = repo.head()?;
-    // let oid = head.target().unwrap();
-    // let commit = repo.find_commit(oid)?;
-    //repo.branch(&branch, target: &Commit<'_>, force: bool)
-
-    // for x in remote.fetch_refspecs()?.iter() {
-    //     println!("{}", x.unwrap().red().bold());
-    // }
-    //repo.remote_add_fetch(name: &str, spec: &str)
-    //remote.fetch(&["master"], Some(opts), None)?;
+    if !found {
+        bail!("failed to find the package in any repo: {}", &pkg);
+    }
 
     Ok(())
 }
+
+// /// Get the repo the given `pkg` lives in.
+// #[cfg(feature = "_arch_")]
+// pub fn repo<T: AsRef<str>>(pkg: T) -> Result<Repo> {
+//     let branch = "packages/pkgfile".to_string();
+//     let refspec = format!("+refs/heads/{0:}:refs/remotes/origin/{0:}", &branch);
+
+//     let repo = Repository::init(&path)?;
+//     for x in &vec![REPO_PACKAGES_NAME, REPO_COMMUNITY_NAME] {
+//         let mut remote = repo.remote(x, &format!("https://git.archlinux.org/svntogit/{}.git", x))?;
+
+//         // Fetch the remote branch via a refspec
+//         remote.fetch(&[&refspec], None, None).unwrap();
+
+//         //repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))?;
+//         let head = repo.find_reference("FETCH_HEAD")?;
+//         let oid = head.target().unwrap();
+//         let commit = repo.find_commit(oid)?;
+//         repo.branch(&branch, &commit, false)?;
+//         // println!("{:?}", oid);
+//         println!("foo");
+//     }
+// }
 
 // Unit tests
 // -------------------------------------------------------------------------------------------------
